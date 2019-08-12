226ffcc2206719006838206c5a269549cd211a16
German Gomez <germandg99@gmail.com>
2019-08-11 20:51:05 -0500

    Creation of projects trought api

--- projects/serializers.py
+++ projects/serializers.py
@@ -3,9 +3,8 @@ from .models import Project
 from user.serielizers import UserSerializer
 from django.contrib.auth.models import User
 
-class ProjectSerializer(serializers.Serializer):
-    
-    name = serializers.CharField(max_length=100)
-    description = serializers.CharField(max_length=500)
-    queryset = User.objects.all()
-    user = serializers.PrimaryKeyRelatedField(queryset=queryset, many=False)
+class ProjectSerializer(serializers.ModelSerializer):
+    user = User.objects.all()
+    class Meta:
+        model = Project
+        fields = ['name', 'description', 'user']
--- projects/views.py
+++ projects/views.py
@@ -14,9 +14,26 @@ class ListProjects(APIView):
     permission_classes = [permissions.IsAuthenticated]
     
     def get(self, request, format=None):
-        
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        data = {
+            'name':request.data.get('name'),
+            'description':request.data.get('description'),
+            'user':request.user.id
+        }
+
+        # validate if data is corrected
+        serializer = ProjectSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            project_saved = serializer.save()
+            context = {'success': 'Project {} has been created successfully'.format(project_saved.name)}
+        else:
+            context = {'error': 'Project could not be created'}
+
+        return Response(context)
+        
 
 
