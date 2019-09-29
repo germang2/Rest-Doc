44fda9e52083b79a44567f616492675f1f31b72d
German Gomez <germandg99@gmail.com>
2019-09-29 18:11:09 -0500

    Create models: Header and Method and had been related with Service. Fixed relationship between Service and Response

--- services/models.py
+++ services/models.py
@@ -10,12 +10,16 @@ class StatusCode(models.Model):
     def __str__(self):
         return str(self.code)
 
-class Response(models.Model):
-    status_code = models.ForeignKey(StatusCode, on_delete=models.PROTECT)
-    json_data = JSONField()
 
-    def __str__(self):
-        return str(self.status_code.code)
+class Method(models.Model):
+    verb = models.CharField(max_length=12)
+    description = models.CharField(max_length=100, blank=True)
+
+
+class Header(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.CharField(max_length=200)
+
 
 class Service(models.Model):
     name = models.CharField(max_length=50)
@@ -23,10 +27,20 @@ class Service(models.Model):
     end_point = models.CharField(max_length=200)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     json_data = JSONField()
-    responses = models.ManyToManyField(Response)
+    method = models.ForeignKey(Method, on_delete = models.PROTECT)
+    headers = models.ManyToManyField(Header)
 
     def __str__(self):
         return self.name
+
+
+class Response(models.Model):
+    status_code = models.ForeignKey(StatusCode, on_delete=models.PROTECT)
+    json_data = JSONField()
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.status_code.code)
 
 
 class Group(models.Model):
