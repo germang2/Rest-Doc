aa490b0326cc4c0350d0945a5476eae62a52b860
German Gomez <germandg99@gmail.com>
2019-10-02 22:29:28 -0500

    Serializer for depedency model

---
+++ dependencies/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Dependency
+
+class DependencySerializer(serializers.ModelSerializer):
+    service = serializers.HyperlinkedIdentityField(
+        many=False,
+        view_name='service-detail'
+    )
+    destination_service = serializers.HyperlinkedIdentityField(
+        many=False,
+        view_name='service-detail'
+    )
+    class Meta:
+        model = Dependency
+        fields = ['service', 'field', 'destination_service']
